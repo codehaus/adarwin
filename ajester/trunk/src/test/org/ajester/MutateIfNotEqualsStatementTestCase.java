a21e4a56f15d57103ad7db89b64fd8d8a365c1c6
stacycurl <stacycurl@a5b8cd60-e6c6-0310-bc19-dd13ed8fe770>
2004-01-06 16:04:18 +0000

    Refactoring model classes to implement Model

---
+++ ajester/trunk/src/test/org/ajester/MutateIfNotEqualsStatementTestCase.java
@@ -0,0 +1,22 @@
+package org.ajester;
+
+import org.ajester.testmodel.code.IfNotEqualsStatement;
+import org.ajester.testmodel.test.IfNotEqualsStatementTestCase;
+
+import junit.framework.TestCase;
+import junit.framework.TestFailure;
+
+public class MutateIfNotEqualsStatementTestCase extends TestCase {
+	public void testMutatingIFNECausesIfStatementIfNotEqualToFail() throws Exception {
+		Report report = new TestRunnerWrapper().run(IfNotEqualsStatementTestCase.class,
+				new IfStatementMutator(IfNotEqualsStatement.LOCATION));
+		
+		assertEquals(1, report.getFailures().size());
+		TestFailure failure = (TestFailure) report.getFailures().toArray()[0];
+		Util.assertEqualsTestCase(failure, IfNotEqualsStatementTestCase.class,
+			"testIfNotEqualMethodReturnsTrue");
+		assertEquals(0, report.getErrors().size());
+		
+		assertTrue(report.getCoverage().contains(IfNotEqualsStatement.LOCATION));
+	}
+}
