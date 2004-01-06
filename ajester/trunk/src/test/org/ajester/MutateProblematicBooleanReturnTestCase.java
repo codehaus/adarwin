3a8bf454cafe46c9e63d11dff0efa89807bd9046
stacycurl <stacycurl@a5b8cd60-e6c6-0310-bc19-dd13ed8fe770>
2004-01-06 17:00:15 +0000

    Refactoring model classes to implement Model

--- ajester/trunk/src/test/org/ajester/MutateProblematicBooleanReturnTestCase.java
+++ ajester/trunk/src/test/org/ajester/MutateProblematicBooleanReturnTestCase.java
@@ -9,7 +9,7 @@ public class MutateProblematicBooleanReturnTestCase extends TestCase {
 	public void testMutatingIrrelevantMethodLeavesTestsStillPassing() throws Exception {
 		Report report = new TestRunnerWrapper().run(
 			ProblematicBooleanReturnTestCase.class,
-			new IfStatementMutator(ProblematicBooleanReturn.IRRELEVANT_METHOD_LOCATION));
+			new IfStatementMutator(ProblematicBooleanReturn.LOCATION));
 
 		assertEquals(0, report.getFailures().size());
 		assertEquals(0, report.getErrors().size());
--- ajester/trunk/src/test/org/ajester/testmodel/code/ProblematicBooleanReturn.java
+++ ajester/trunk/src/test/org/ajester/testmodel/code/ProblematicBooleanReturn.java
@@ -2,8 +2,8 @@ package org.ajester.testmodel.code;
 
 import org.ajester.CodeLocation;
 
-public class ProblematicBooleanReturn {
-	public static final CodeLocation IRRELEVANT_METHOD_LOCATION =
+public class ProblematicBooleanReturn implements Model {
+	public static final CodeLocation LOCATION =
 		new CodeLocation(ProblematicBooleanReturn.class, "irrelevantMethod");
 	
 	public boolean getTrue() {
